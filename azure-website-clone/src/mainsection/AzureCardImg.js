e6d6c9f471a10b34b08fab10c3e5ff6987aab298
Theres Mary <theresmaryjohn@gmail.com>
2023-05-23 12:05:48 +0530

    feat:mainsection started

--- azure-website-clone/src/mainsection/AzureCardImg.js
+++ azure-website-clone/src/mainsection/AzureCardImg.js
@@ -1,29 +1,37 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from "react";
+import "./AzureCardImg.css";
 
+function AzureCardImg() {
+  const [data, setData] = useState([]);
 
- function AzureCardImg() {
-    const [data,setData]=useState([]);
-
-    const fetchData=()=>{
-      fetch("https://raw.githubusercontent.com/theresmaryjohn/react-website/main/azure-website-clone/public/data.json")
-      .then((response)=>{
+  const fetchData = () => {
+    fetch(
+      "https://raw.githubusercontent.com/theresmaryjohn/react-website/main/azure-website-clone/public/data.json"
+    )
+      .then((response) => {
         return response.json();
       })
-      .then((data)=>{
-       console.log(data);
-       setData(data.datas)
-      })
-    }
+      .then((data) => {
+        console.log(data);
+        setData(data.datas);
+      });
+  };
 
-    useEffect(()=>{
-     fetchData();
-    },[])
-
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
-    <div className='mainCard'>
-      <img src={data.imgurlmobile} />
+    <div className="mainCard-section">
+      <div className="card-img-text">
+        <img src={data.imgurlmobile} />
+        <div className="mainCard">
+          <h1 className="main-head">{data.headazure}</h1>
+          <h1 className="main-head-two">{data.headAI}</h1>
+          <p>{data.textstart}</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 export default AzureCardImg;
