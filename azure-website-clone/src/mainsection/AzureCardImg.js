b08cd44859ec3954db6f95dc3236ae759859fe69
Theres Mary <theresmaryjohn@gmail.com>
2023-05-23 13:49:35 +0530

    feat:header lap

--- azure-website-clone/src/mainsection/AzureCardImg.js
+++ azure-website-clone/src/mainsection/AzureCardImg.js
@@ -29,6 +29,22 @@ function AzureCardImg() {
           <h1 className="main-head">{data.headazure}</h1>
           <h1 className="main-head-two">{data.headAI}</h1>
           <p>{data.textstart}</p>
+          <div className="buttons">
+            <a className="getstarted">Get started</a>
+            <a className="tryazure">Try Azure for free</a>
+          </div>
+        </div>
+      </div>
+      <div className="card-img-text-lap">
+      <img src={data.imgurldesktop} />
+        <div className="mainCard">
+          <h1 className="main-head">{data.headazure}</h1>
+          <h1 className="main-head-two">{data.headAI}</h1>
+          <p>{data.textstart}</p>
+          <div className="buttons">
+            <a className="getstarted">Get started</a>
+            <a className="tryazure">Try Azure for free</a>
+          </div>
         </div>
       </div>
     </div>
