d46d1ff5e99317f1cb1f786ce2faea88b74a47b9
Theres Mary <theresmaryjohn@gmail.com>
2023-06-21 15:01:50 +0530

    feat:deleted flipkart-clone folder

--- flipkart-clone/src/fetchData.js
+++
@@ -1,7 +0,0 @@
-const fetchData = async () => {
-    const response = await fetch("./data.json");
-    const parsedResponse = await response.json();
-    return parsedResponse;
-  };
-
-  export default fetchData;
